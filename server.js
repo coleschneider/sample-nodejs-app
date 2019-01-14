c69ce756811f74d246ed0cbe9d1c930764d8f9ef
Cole Schneider <colekschneider@gmail.com>
2019-01-14 00:34:52 -0800

    change docker image name

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world from a Node.js app!')
+    res.send('new change!')
 })
 app.listen(3000, () => {
     console.log('Server is up on 3000')
